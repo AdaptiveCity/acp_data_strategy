6c0eca01350e928b2e797aed59dda6e6f15510b2
rv355 <rv355@cam.ac.uk>
2020-06-02 19:22:03 +0100

    Updated dbconn

--- dbconn.py
+++ dbconn.py
@@ -3,10 +3,7 @@ from CONFIG import *
 
 def dbread(query):
     con = psycopg2.connect(database=PGDATABASE,
-                            user=PGUSER,
-                            password=PGPASSWORD,
-                            host=PGHOST,
-                            port=PGPORT)
+                            user=PGUSER)
 
     cur = con.cursor()
     cur.execute(query)
@@ -16,10 +13,7 @@ def dbread(query):
 
 def dbwrite(query):
     con = psycopg2.connect(database=PGDATABASE,
-                            user=PGUSER,
-                            password=PGPASSWORD,
-                            host=PGHOST,
-                            port=PGPORT)
+                            user=PGUSER)
 
     cur = con.cursor()
     cur.execute(query)
